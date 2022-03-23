8034b9607ab72102723f4456b0b738b76151bf3a
dodicandra <dodicandra20@gmail.com>
2022-03-23 15:14:39 +0700

    v1.4.0

--- lib/Modal.d.ts
+++ lib/Modal.d.ts
@@ -5,12 +5,14 @@ interface Modal {
     open(): void;
 }
 export interface ModalProps {
-    size?: 's' | 'm' | 'l' | 'xl';
+    size?: 's' | 'm' | 'l' | 'xl' | 'full';
     dragColor?: ColorValue;
     dragShadowColor?: ColorValue;
     disableDrag?: boolean;
+    backDropColor?: string;
     onClosed?(): void;
-    children: (() => React.ReactNode) | React.ReactNode[] | React.ReactNode;
 }
-declare const Modal: React.MemoExoticComponent<React.ForwardRefExoticComponent<ModalProps & React.RefAttributes<Modal>>>;
+declare const Modal: React.MemoExoticComponent<React.ForwardRefExoticComponent<ModalProps & {
+    children?: React.ReactNode;
+} & React.RefAttributes<Modal>>>;
 export default Modal;
